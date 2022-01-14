46ff1f3169b7539e875264099095cacb6031a11e
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2022-01-14 17:05:29 +0300

    simple reactive policy

--- sgd_env/policy/__init__.py
+++ sgd_env/policy/__init__.py
@@ -1,6 +1,7 @@
 from sgd_env.policy.policy import AbstractPolicy
 from sgd_env.policy.policy import ConstantLRPolicy
 from sgd_env.policy.policy import CosineAnnealingLRPolicy
+from sgd_env.policy.policy import SimplePolicy
 
 
-__all__ = ["AbstractPolicy", "ConstantLRPolicy", "CosineAnnealingLRPolicy"]
+__all__ = ["AbstractPolicy", "ConstantLRPolicy", "CosineAnnealingLRPolicy", "SimplePolicy"]
--- sgd_env/policy/policy.py
+++ sgd_env/policy/policy.py
@@ -1,6 +1,7 @@
 import dataclasses
 from abc import ABC, abstractmethod
 from ast import literal_eval
+from typing import Optional
 
 import numpy as np
 
@@ -30,6 +31,9 @@ class AbstractPolicy(ABC):
 class ConstantLRPolicy(AbstractPolicy):
     lr: float
 
+    def __post_init__(self):
+        assert self.lr > 0
+
     def act(self, _):
         return self.lr
 
@@ -49,6 +53,9 @@ class CosineAnnealingLRPolicy(AbstractPolicy):
     lr: float
     steps: int = dataclasses.field(init=False)
 
+    def __post_init__(self):
+        assert self.lr > 0
+
     def act(self, state):
         return 0.5 * (1 + np.cos(state["step"] * np.pi / self.steps)) * self.lr
 
@@ -61,3 +68,42 @@ class CosineAnnealingLRPolicy(AbstractPolicy):
 
     def reset(self, instance: generators.Instance):
         self.steps = instance.steps
+        assert self.steps > 0
+
+
+@dataclasses.dataclass
+class SimplePolicy(AbstractPolicy):
+    lr: float
+    a: float
+    b: float
+    steps: int = dataclasses.field(init=False)
+    loss: float = dataclasses.field(init=False)
+    prev_loss: Optional[float] = dataclasses.field(init=False)
+    epoch_size: int = dataclasses.field(init=False)
+
+    def __post_init__(self):
+        assert self.lr > 0
+        assert 0 < self.a <= 1
+        assert 0 < self.b <= 1
+
+    def act(self, state):
+        self.loss += state['loss'].sum()
+        if not (state['step'] + 1) % self.epoch_size:
+            if self.prev_loss is not None:
+                self.lr *= self.a if self.loss > self.prev_loss else 1/self.b
+            self.prev_loss = self.loss
+            self.loss = 0.0
+        return self.lr
+
+    def load(self):
+        return str(dataclasses.asdict(self))
+
+    @classmethod
+    def save(cls, pi_str):
+        return cls(**literal_eval(pi_str))
+
+    def reset(self, instance: generators.Instance):
+        self.steps = instance.steps
+        self.loss = 0.0
+        self.prev_loss = None
+        self.epoch_size = len(instance.loaders[0])
