760a29f790d86b7c1422195de3d070063688791d
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-03-18 13:36:40 +0100

    Add path argument to load_solution signature
    As required by the interface in dac4automlcomp submission template.

--- examples/rl_for_dac/solution.py
+++ examples/rl_for_dac/solution.py
@@ -1,12 +1,14 @@
 import os
+from pathlib import Path
+from typing import Union
 
 from dac4automlcomp.policy import DACPolicy
 
 from policy import SGDPolicy
 
-model_fn = os.path.abspath(os.path.join(os.path.dirname(__file__), "tmp/model.zip"))
 
-
-def load_solution() -> DACPolicy:
+def load_solution(path: Union[str, Path] = Path(".")) -> DACPolicy:
+    path = Path(path)
+    model_fn = path / "tmp/model.zip"
     sgd_policy = SGDPolicy.load(model_fn)
     return sgd_policy
